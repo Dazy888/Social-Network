eaf6bcb50aaa24e3a36bbf2a6bd00809bcfe8d5a
unknown <8205414@gmail.com>
2023-01-09 23:00:06 +0200

    Added css media for 404 page

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -1,16 +1,21 @@
+import React from "react"
 import { useRouter } from "next/router"
+// @ts-ignore
 import styles from '../styles/Not-Found.module.scss'
-export default function NoContent() {
+const NoContent = () => {
     const router = useRouter()
 
     return(
-        <div className={styles['content']}>
-            <div className={`${styles['content-container']} flex-between`}>
-                <h1>404</h1>
-                <hr/>
-                <p>Something went wrong, go back to the main page</p>
+        <div className={`${styles['content']} flex-center`}>
+            <div>
+                <div className={`${styles['content-container']} flex-between`}>
+                    <h1>404</h1>
+                    <hr/>
+                    <p>Something went wrong, go back to the main page</p>
+                </div>
+                <button onClick={() => router.push('/main/profile')}>Go Home</button>
             </div>
-            <button onClick={() => router.push('/main/profile')}>Go Home</button>
         </div>
     )
 }
+export default React.memo(NoContent)
