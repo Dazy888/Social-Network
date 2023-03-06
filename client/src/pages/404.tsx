83b11c568e0c58949d6d18fc83f04a8c7fdb18e6
unknown <8205414@gmail.com>
2023-03-06 15:00:32 +0200

    Added Tailwind on the 404 page

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -6,16 +6,17 @@ const NoContent = () => {
     const router = useRouter()
 
     return(
-        <div className={`${styles['content']} flex-center`}>
+        <div className={`${styles['content']} flex justify-center items-center h-screen text-white font-medium`}>
             <div>
-                <div className={`${styles['content-container']} flex-between`}>
-                    <h1>404</h1>
-                    <hr/>
-                    <p>Something went wrong, go back to the main page</p>
+                <div className={`${styles['text']} flex justify-between items-center`}>
+                    <h1 className={'text-5xl'}>404</h1>
+                    <hr className={'w-0.5 h-12'}/>
+                    <p className={'text-2xl max-w-sm'}>Something went wrong, go back to the main page</p>
                 </div>
-                <button onClick={() => router.push('/main/profile')}>Go Home</button>
+                <button className={'w-48 h-12 text-xl mt-12 mx-auto block duration-300 rounded-lg'} onClick={() => router.push('/main/profile')}>Go Home</button>
             </div>
         </div>
     )
 }
+
 export default React.memo(NoContent)
