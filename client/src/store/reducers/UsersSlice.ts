5a65be3d6b2ddfe45bdc24d10cdc93f74c249e98
Dazy888 <8205414@gmail.com>
2023-07-14 20:57:18 -0300

    Edited UsersSlice

--- client/src/store/reducers/UsersSlice.ts
+++ client/src/store/reducers/UsersSlice.ts
@@ -2,33 +2,35 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { PublicUserData } from "@/models/users.models"
 
 interface UsersState {
-    openedUser: PublicUserData
+    // openedUser: PublicUserData
 }
 
 let initialState: UsersState = {
-    openedUser: {
-        name: '',
-        skills: '',
-        location: '',
-        hobbies: '',
-        posts: [],
-        following: [],
-        followers: [],
-        avatar: '',
-        aboutMe: '',
-        banner: ''
-    }
+    // openedUser: {
+    //     name: '',
+    //     location: '',
+    //     avatar: '',
+    //     banner: '',
+    //     aboutMe: '',
+    //     hobbies: '',
+    //     skills: '',
+    //     posts: [],
+    //     subscriptions: {
+    //         followings: [],
+    //         followers: []
+    //     }
+    // }
 }
 
 export const usersSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {
-        setOpenedUser(state, action: PayloadAction<PublicUserData>) {
-            state.openedUser = action.payload
-        }
+        // setOpenedUser(state, action: PayloadAction<PublicUserData>) {
+        //     state.openedUser = action.payload
+        // }
     }
 })
 
-export const { setOpenedUser } = usersSlice.actions
+// export const { setOpenedUser } = usersSlice.actions
 export const usersReducer = usersSlice.reducer
