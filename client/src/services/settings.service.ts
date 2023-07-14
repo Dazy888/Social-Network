c60e9113d96ee60dd3d1578a3aab2cb4e0d783a5
Dazy888 <8205414@gmail.com>
2023-07-14 20:53:59 -0300

    Updated Settings service

--- client/src/services/settings.service.ts
+++ client/src/services/settings.service.ts
@@ -2,19 +2,19 @@ import { $api } from "@/http"
 
 export class SettingsService {
     static changePassword(currentPass: string, newPass: string, id: string) {
-        return $api.put('settings/change-pass', { currentPass, newPass, id })
+        return $api.put('settings/changePassword', { currentPass, newPass, id })
             .then(res => res)
             .catch(err => { throw err.response.data.message })
     }
 
     static activateMail(email: string, id: string) {
-        return $api.post('settings/activate-email', { email, id })
+        return $api.post('settings/activateEmail', { email, id })
             .then(() => email)
             .catch(err => { throw err.response.data.message })
     }
 
     static async cancelActivation(id: string) {
-        return $api.delete(`settings/cancel-activation/${id}`)
+        return $api.delete(`settings/cancelEmailActivation/${id}`)
             .then(res => res)
             .catch(err => { throw err.response.data.message })
     }
